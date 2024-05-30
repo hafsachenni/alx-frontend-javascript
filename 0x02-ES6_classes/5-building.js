db665b6c7f650de3d3a5bdf239edfd668b8ccf40
hafsachenni <hafsachenni77@gmail.com>
2024-05-30 15:52:27 +0000

    extends

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
