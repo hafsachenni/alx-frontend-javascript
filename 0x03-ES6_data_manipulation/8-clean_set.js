f748aafe1a9333a6eb9533d3afb355adb09adc50
hafsa chenni <hafsachenni77@gmail.com>
2024-06-03 21:02:42 +0200

    slicing a str into 2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const stringUni = [];
   for (const value of set) {
     if (value.startsWith(startString)) {
