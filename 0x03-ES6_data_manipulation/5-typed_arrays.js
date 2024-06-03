c1d44b9ecba8dc8230134c14815fe80f5fbb0100
hafsa chenni <hafsachenni77@gmail.com>
2024-06-03 19:02:59 +0200

    arraybuffer with an int8 bits value

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
+  // checking if position is outside the valid given range of buffer
   if (position > length - 1) throw Error('Position outside range');
 
   const view = new DataView(buffer);
