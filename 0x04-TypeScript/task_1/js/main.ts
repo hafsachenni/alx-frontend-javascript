32462c23d7b499b2a683525c89254c0cce066630
hafsachenni <hafsachenni77@gmail.com>
2024-06-05 09:38:27 -0700

    created a teacher interface and class

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,24 @@
+interface Teacher{
+	readonly firstName: string;
+	readonly lastName: string;
+	fullTimeEmployee: boolean;
+	yearsOfExperience?: number;
+	location: string;
+	[key: string]: any;
+}
+
+class Teacher3 implements Teacher{
+	readonly firstName: string;
+        readonly lastName: string;
+        fullTimeEmployee: boolean;
+        yearsOfExperience?: number;
+        location: string;
+	
+
+	constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, yearsOfExperience: number, location: string){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.fullTimeEmployee = fullTimeEmployee;
+		this.location = location;
+	}
+}
